55e9c5ebab194b8fd35bb38754404bc70a603452
akiromishidoze <akirodaisuki10@gmail.com>
2026-07-28 09:49:40 +0800

    feat: add SystemSampleDataSeeder to database seeder chain

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CustomerRiskRulesSeeder::class,
             TicketExampleSeeder::class,
+            SystemSampleDataSeeder::class,
         ]);
     }
 }
